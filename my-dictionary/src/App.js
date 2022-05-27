5a113072eb17fde35c831a3a61821b54ffc6d6bc
hongsoom <hongsoom@kakao.com>
2022-05-27 21:56:43 +0900

    path settings

--- my-dictionary/src/App.js
+++ my-dictionary/src/App.js
@@ -1,11 +1,17 @@
 import {db} from "./firebase";
 import React from "react"
+import { Route, Switch } from "react-router-dom";
+import Main from "./component/Main";
+import Add from "./component/Add";
 
 function App() {
 
   return (
     <div className="App">
-
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/add" componet={Add} />
+      </Switch>
     </div>
   );
 }
