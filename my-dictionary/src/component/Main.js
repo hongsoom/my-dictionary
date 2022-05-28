fffa802cf5e3f3ee6435450da167da3f716a0900
hongsoom <hongsoom@kakao.com>
2022-05-28 15:04:31 +0900

    Maing page UI

--- my-dictionary/src/component/Main.js
+++ my-dictionary/src/component/Main.js
@@ -1,26 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
 import Header from "./Header";
 import WordCard from "./WordCard";
 
 const Main = () => {
+    const history = useHistory();
+
     return (
-        <Wrapper>
+        <MainWrap>
             <Header />
                 <Content>hello</Content>
-        </Wrapper>
+                <AddBottom>
+                    <button type="button" onClick={() => {
+                        history.push("/Add");
+                    }}>
+                        +
+                    </button>
+                </AddBottom>
+        </MainWrap>
     )
 }
 
-const Wrapper = styled.div`
+const MainWrap = styled.div`
     padding-top: 60px;
 `;
 
 const Content = styled.div`
     margin: 0 auto;
     margin-top: 2rem;
-    width: 1200px;
+    width: 1000px;
+    height: 900px;
     position: relative;
-    background: gray;
+    border: 1px solid black;
 `;
+
+const AddBottom = styled.div`
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    button {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        margin-bottom: 30px;
+        margin-right: 50px;
+        border: 3px solid #EF9F9F;
+        background-color: #EF9F9F;
+        color: #ffffff;
+        font-size: 40px;
+        cursor:pointer;
+        box-shadow: 2px 2px 2px 2px gray;
+        font-family: 'Jua', sans-serif;
+      }
+`;
+
 export default Main;
