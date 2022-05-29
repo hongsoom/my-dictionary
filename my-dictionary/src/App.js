068c56f1ced916c7f355c4212c3de6ed4fd3302a
hongsoom <hongsoom@kakao.com>
2022-05-29 14:44:00 +0900

    path setting

--- my-dictionary/src/App.js
+++ my-dictionary/src/App.js
@@ -3,6 +3,7 @@ import React from "react"
 import { Route, Switch } from "react-router-dom";
 import Main from "./component/Main";
 import Add from "./component/Add";
+import Edit from "./component/Edit";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Main} />
         <Route exact path="/Add" component={Add} />
+        <Route exact path="/Edit" component={Add} />
       </Switch>
     </div>
   );
