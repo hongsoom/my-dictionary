bfaa9cb3430b9915a863ba209c2934741f2f63c6
hongsoom <hongsoom@kakao.com>
2022-05-30 21:29:22 +0900

    add middlewares

--- my-dictionary/src/redux/modules/words.js
+++ my-dictionary/src/redux/modules/words.js
@@ -1,58 +1,72 @@
 import {db} from "../../firebase"; 
+import { collection, doc, getDoc, getDocs, addDoc, updateDoc, deleteDoc } from "firebase/firestore";
 
 // Actions
 const LOAD   = 'words/LOAD';
 const CREATE = 'words/CREATE';
 const UPDATE = 'words/UPDATE';
-const REMOVE = 'words/REMOVE';
+const DELETE = 'words/DELETE';
 
 // 초기 상태값
 const initialState = {
-    list: [{
-      word : '홍수민홍수민홍수',
-      mean : 'adfdsfdsf수',
-      ex : "asfdadsfdasfads민"
-    },  {
-      word : '12345678',
-      mean : '하',
-      ex : "얀"
-    }, {
-      word : 'javascript',
-      mean : '현',
-      ex : "준"
-    }, {
-      word : '이',
-      mean : '병',
-      ex : "수"
-    }],
+    list: [],
   };
 
 // Action Creators
-export function loadWords(word_list) {
+export function loadWord(word_list) {
+  console.log('1 mount', word_list);
   return { type: LOAD, word_list };
 }
 
-export function createWords(word) {
-  console.log(word);
+export function createWord(word) {
+  console.log('5 객체생성', word);
   return { type: CREATE, word };
 }
 
-export function updateWords(word_index) {
+export function updateWord(word_index) {
   return { type: UPDATE, word_index };
 }
 
-export function removeWords(word_index) {
-  return { type: REMOVE, word_index };
+export function deleteWord(word_index) {
+  return { type: DELETE, word_index };
+}
+
+// middlewares
+export const loadWordFB = () => {
+  return async function (dispatch) {
+    const word_data = await getDocs(collection(db, "words"));
+
+    let word_list = [];
+
+    word_data.forEach((doc) => {
+      word_list.push({id:doc.id, ...doc.data()});
+    });
+
+    dispatch(loadWord(word_list));
+    console.log('2 미들웨어', word_list);
+  }
+}
+
+export const createWordFB = (word) => {
+  return async function (dispatch) {
+    const docRef = await addDoc(collection(db, "words"), word);
+    const word_data = {id: docRef.id, ...word};
+
+    dispatch(createWord(word_data))
+    console.log('4 미들웨어', word_data);
+  }
 }
 
 // reducer
 export default function reducer(state = initialState, action = {}) {
     switch (action.type) {
       case "words/LOAD":
+        console.log('3 리듀서', action.word_list);
         return {list : action.word_list};
   
       case "words/CREATE": {
         const new_word_list = [...state.list, action.word];
+        console.log('6 리듀서', new_word_list);
         return { list: new_word_list };
       }
     
