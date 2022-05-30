7363369b903d8af72235a2aa93aa2ee8485dfc7a
hongsoom <hongsoom@kakao.com>
2022-05-30 21:30:15 +0900

    add path edit and useEffect

--- my-dictionary/src/App.js
+++ my-dictionary/src/App.js
@@ -1,18 +1,24 @@
-import {db} from "./firebase";
 import React from "react"
 import { Route, Switch } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { loadWordFB, deleteWords } from "../src/redux/modules/words";
 import Main from "./component/Main";
 import Add from "./component/Add";
 import Edit from "./component/Edit";
 
 function App() {
+  const dispatch = useDispatch();
+
+  React.useEffect (() => {
+    dispatch(loadWordFB());
+  }, []);
 
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Main} />
         <Route exact path="/Add" component={Add} />
-        <Route exact path="/Edit" component={Add} />
+        <Route exact path="/Edit" component={Edit} />
       </Switch>
     </div>
   );
